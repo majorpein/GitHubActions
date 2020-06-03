8834bd5cb15091a8928b50f7ec0cc8b3511cd33e
Aleksandr Anosov <aanosov@moysklad.ru>
2020-06-03 09:48:57 +0300

    Tests

--- GitHubActionsTests/GitHubActionsTests.swift
+++ GitHubActionsTests/GitHubActionsTests.swift
@@ -19,16 +19,19 @@ class GitHubActionsTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testUnitExample1() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testUnitExample2() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testUnitExample3() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
 }
