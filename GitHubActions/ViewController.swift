716c8126189acee45467a4f8dc343bff7acaef95
Aleksandr Anosov <aanosov@moysklad.ru>
2020-06-03 09:42:17 +0300

    Initial Commit

---
+++ GitHubActions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubActions
+//
+//  Created by Александр Аносов on 03.06.2020.
+//  Copyright © 2020 Anosov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
